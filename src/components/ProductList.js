60696c3a5ce8e3436c535017ae8b4cc457a1d759
handesaurabh <handesaurabh2004@gmail.com>
2026-01-01 12:07:49 +0530

    newly changes

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,32 +1,44 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { addToCart } from "../actions/cartActions";
-import { addToWishlist } from "../actions/wishlistActions";
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addToCart } from '../actions/cartActions';
+import { addToWishlist, removeFromWishlist } from '../actions/wishlistActions';
 
 const ProductList = ({ products }) => {
   const dispatch = useDispatch();
+  const wishlistItems = useSelector(state => state.wishlist.items);
+
+  const isInWishlist = (id) =>
+    wishlistItems.some(item => item.id === id);
 
   return (
     <div className="row">
       {products.map(product => (
         <div key={product.id} className="col-md-4 mb-4">
           <div className="custom-card card">
-            <h4>{product.name}</h4>
-            <p>${product.price}</p>
+            <div className="card-body">
+              <h4>{product.name}</h4>
+              <p>${product.price}</p>
 
-            <button
-              className="add-to-cart"
-              onClick={() => dispatch(addToCart(product))}
-            >
-              Add to Cart
-            </button>
+              <button
+                className="btn btn-primary mb-2"
+                onClick={() => dispatch(addToCart(product))}
+              >
+                Add to Cart
+              </button>
 
-            <button
-              className="add-to-wishlist"
-              onClick={() => dispatch(addToWishlist(product))}
-            >
-              Add to Wishlist
-            </button>
+              <button
+                className="btn btn-secondary"
+                onClick={() =>
+                  isInWishlist(product.id)
+                    ? dispatch(removeFromWishlist(product.id))
+                    : dispatch(addToWishlist(product))
+                }
+              >
+                {isInWishlist(product.id)
+                  ? 'Remove from Wishlist'
+                  : 'Add to Wishlist'}
+              </button>
+            </div>
           </div>
         </div>
       ))}
--- src/components/ProductsPage.js
+++ src/components/ProductsPage.js
@@ -1,15 +1,23 @@
-import React from "react";
-import products from "../products";
-import ProductList from "./ProductList";
-import Cart from "./Cart";
-import Wishlist from "./Wishlist";
+import React from 'react';
+import products from '../products';
+import ProductList from './ProductList';
+import Cart from './Cart';
+import Wishlist from './Wishlist';
 
 const ProductsPage = () => {
   return (
-    <div className="container">
-      <ProductList products={products} />
-      <Cart />
-      <Wishlist />
+    <div className="app">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="text-center w-100">
+          <h1 className="display-4">Shopping Cart</h1>
+        </div>
+      </nav>
+
+      <main className="container mt-4">
+        <ProductList products={products} />
+        <Cart />
+        <Wishlist />
+      </main>
     </div>
   );
 };
