a576f538ac59f5cbb0a63a74b8b6c753a4a94c53
handesaurabh <handesaurabh2004@gmail.com>
2026-01-01 12:25:15 +0530

    app changed

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,12 @@ import "../styles/App.css";
 const App = () => {
   return (
     <Provider store={store}>
+      <nav className="navbar-expand-lg">
+        <div className="text-center">Shopping Cart</div>
+      </nav>
+
       <div></div>
+
       <div>
         <h3>All Products</h3>
         <ProductsPage />
